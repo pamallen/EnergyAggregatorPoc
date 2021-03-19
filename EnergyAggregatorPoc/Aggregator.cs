56425561974b8514a3d13e32523262ebdc37d9d7
Pierre-Alexis Mallen <pamallen@hotmail.fr>
2021-03-19 19:21:53 +0100

    Finished basic aggregator (with hacks... for now)

--- EnergyAggregatorPoc/Aggregator.cs
+++ EnergyAggregatorPoc/Aggregator.cs
@@ -27,13 +27,43 @@ namespace EnergyAggregatorPoc
             PowerFactories.Add(powerFactory);
         }
 
-        public Task<List<EnergyReading>> AggregateReadings(DateTime startTime, DateTime endTime, string format)
+        public async Task<List<EnergyReading>> AggregateReadings(DateTime startTime, DateTime endTime, string format)
         {
-            PowerFactories.ForEach(async pf =>
-            {
-                await pf.GetEnergyProductions(startTime, endTime);
-            });
+            //PowerFactories.ForEach(async pf =>
+            //{
+            //    await pf.GetEnergyProductions(startTime, endTime);
+            //});
+            await PowerFactories[0].GetEnergyProductions(startTime, endTime);
+            await PowerFactories[1].GetEnergyProductions(startTime, endTime);
 
+            var aggregatedResults = new List<EnergyReading>();
+            var currentStartTime = startTime;
+            var currentEndTime = startTime + TimeStep;
+
+
+            while (currentStartTime < endTime)
+            {
+                double currentAggregatedPower = 0;
+                PowerFactories.ForEach(pf =>
+                {
+                    var currentEnergyProduction = pf.EnergyProductions.Find(ep => ep.StartTime <= currentStartTime && ep.EndTime >= currentEndTime);
+                    currentAggregatedPower += currentEnergyProduction?.Power ?? 0;
+                });
+
+                var currentAggregatedReading = new EnergyReading
+                {
+                    Start = currentStartTime.Subtract(DateTime.UnixEpoch).Ticks,
+                    End = currentEndTime.Subtract(DateTime.UnixEpoch).Ticks,
+                    Power = currentAggregatedPower
+                };
+
+                aggregatedResults.Add(currentAggregatedReading);
+
+                currentStartTime += TimeStep;
+                currentEndTime += TimeStep;
+            }
+
+            return aggregatedResults;
         }
     }
 }
--- EnergyAggregatorPoc/Program.cs
+++ EnergyAggregatorPoc/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace EnergyAggregatorPoc
 {
@@ -7,19 +8,19 @@ namespace EnergyAggregatorPoc
     {
         static async Task Main(string[] args)
         {
-            double.TryParse(args[0], out double startTimeStamp);
-            double.TryParse(args[1], out double endTimeStamp);
+            DateTime.TryParse(args[0], out DateTime startTime);
+            DateTime.TryParse(args[1], out DateTime endTime);
 
             var aggregator = Aggregator.Create(TimeSpan.FromMinutes(15));
 
             aggregator.AddFactory(PowerFactory.Create("https://interview.beta.bcmenergy.fr/hawes", "JSON", TimeSpan.FromMinutes(15)));
             aggregator.AddFactory(PowerFactory.Create("https://interview.beta.bcmenergy.fr/barnsley", "JSON", TimeSpan.FromMinutes(30)));
-            aggregator.AddFactory(PowerFactory.Create("https://interview.beta.bcmenergy.fr/hounslow", "CSV", TimeSpan.FromMinutes(60)));
+            //aggregator.AddFactory(PowerFactory.Create("https://interview.beta.bcmenergy.fr/hounslow", "CSV", TimeSpan.FromMinutes(60)));
 
 
-            await aggregator.AggregateReadings(DateTime.UnixEpoch.AddSeconds(startTimeStamp),
-                DateTime.UnixEpoch.AddSeconds(endTimeStamp), args[2]);
-            Console.WriteLine("Hello World!");
+            var result = JsonConvert.SerializeObject(await aggregator.AggregateReadings(startTime, endTime, args[2]), Formatting.Indented);
+
+            Console.WriteLine(result);
         }
     }
 }
